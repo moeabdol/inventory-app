493a2bd5b957993cc1d1159c4063458c5723f183
moeabdol <mohd.a.saed@gmail.com>
2017-08-24 20:34:41 +0300

    Product list in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+import { Product } from "./product.model";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  products: Product[];
+
+  constructor() {
+    this.products = [
+      new Product(
+        "MySHOES",
+        "Black Runing Shoes",
+        "/assets/images/products/black-shoes.jpg",
+        ["Men", "Shoes", "Running Shoes"],
+        109.99
+      ),
+      new Product(
+        "NEATOJACKET",
+        "Blue Jacket",
+        "/assets/images/products/blud-jacket.jpg",
+        ["Women", "Apparel", "Jackets & Vests"],
+        238.99
+      ),
+      new Product(
+        "NICEHAT",
+        "A Nice Black Hat",
+        "/assets/images/products/black-hat.jpg",
+        ["Men", "Accessories", "Hats"],
+        29.99
+      )
+    ];
+  }
 }
