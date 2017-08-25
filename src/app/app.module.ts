502fd29a84dd0307cd178688f4472d37bbe92abd
moeabdol <mohd.a.saed@gmail.com>
2017-08-25 18:36:44 +0300

    Implement product-list.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductsListComponent } from './products-list/products-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/products-list/products-list.component.ts
@@ -0,0 +1,35 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output
+} from "@angular/core";
+
+import { Product } from "../product.model";
+
+@Component({
+  selector: "products-list",
+  templateUrl: "./products-list.component.html",
+  styleUrls: ["./products-list.component.css"]
+})
+export class ProductsListComponent {
+  @Input() productList: Product[];
+  @Output() onProductSelected: EventEmitter<Product>;
+  private currentProduct: Product;
+
+  constructor() {
+    this.onProductSelected = new EventEmitter();
+  }
+
+  clicked(product: Product): void {
+    this.currentProduct = product;
+    this.onProductSelected.emit(product);
+  }
+
+  isSelected(product: Product): boolean {
+    if (!product || !this.currentProduct) {
+      return false;
+    }
+    return product.sku === this.currentProduct.sku;
+  }
+}
