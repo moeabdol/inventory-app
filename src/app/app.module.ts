ae72a460995fa20e10ad2c2baeed22cd03d76b58
moeabdol <mohd.a.saed@gmail.com>
2017-08-25 18:47:37 +0300

    Implement product-row.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductRowComponent } from './product-row/product-row.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductsListComponent
+    ProductsListComponent,
+    ProductRowComponent
   ],
   imports: [
     BrowserModule
