b3bc86a629c045015021175ab77e674b39d0e163
moeabdol <mohd.a.saed@gmail.com>
2017-08-25 20:01:01 +0300

    Implement product-image.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ProductsListComponent } from './products-list/products-list.component';
 import { ProductRowComponent } from './product-row/product-row.component';
+import { ProductImageComponent } from './product-image/product-image.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsListComponent,
-    ProductRowComponent
+    ProductRowComponent,
+    ProductImageComponent
   ],
   imports: [
     BrowserModule
