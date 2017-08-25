b5415a86492246747b16d44f66bcc962b36243d8
moeabdol <mohd.a.saed@gmail.com>
2017-08-25 20:51:21 +0300

    Fix typo in products-list.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { ProductsListComponent } from './products-list/products-list.component';
-import { ProductRowComponent } from './product-row/product-row.component';
-import { ProductImageComponent } from './product-image/product-image.component';
-import { PriceDisplayComponent } from './price-display/price-display.component';
-import { ProductDepartmentComponent } from './product-department/product-department.component';
+import { AppComponent } from "./app.component";
+import { ProductsListComponent } from "./products-list/products-list.component";
+import { ProductRowComponent } from "./product-row/product-row.component";
+import { ProductImageComponent } from "./product-image/product-image.component";
+import { PriceDisplayComponent } from "./price-display/price-display.component";
+import { ProductDepartmentComponent } from "./product-department/product-department.component";
 
 @NgModule({
   declarations: [
