322edc302962d2666afb19dc5d550e72f3b31676
moeabdol <mohd.a.saed@gmail.com>
2017-08-25 20:09:42 +0300

    Implement price-display.component

---
+++ src/app/price-display/price-display.component.ts
@@ -0,0 +1,13 @@
+import {
+  Component,
+  Input
+} from "@angular/core";
+
+@Component({
+  selector: "price-display",
+  templateUrl: "./price-display.component.html",
+  styleUrls: ["./price-display.component.css"]
+})
+export class PriceDisplayComponent {
+  @Input() price: number;
+}
